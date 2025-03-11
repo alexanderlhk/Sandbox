330069c9714b1fc8dd33f31e3bd47465698414f2
Alexander <alexanderlhk@gmail.com>
2025-03-11 15:05:47 +0800

    added nullPractice.js

---
+++ nullPractice.js
@@ -0,0 +1,14 @@
+// What is Null - https://www.altcademy.com/blog/what-is-null-in-javascript/
+
+// Null
+const nullVar = null;
+console.log(nullVar); //null
+console.log(typeof(nullVar)); //object
+
+// Undefined
+const undefinedVar = undefined;
+console.log(undefinedVar); //undefined
+console.log(typeof(undefinedVar)); //undefined
+
+// One common pitfall for new developers is to confuse null with undefined. While both seem to indicate emptiness or absence of value, they are not the same.
+// Consider undefined as an empty plot of land where nothing has been built yet. On the other hand, null is like a plot where a building was planned but never built. The foundation was laid, but there's no building there.
